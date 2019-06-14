e203d4299342225a3c5692ad49de46e0d5f54a6f
WEBCONSOL Inc <info@webconsolinc.com>
2019-06-14 16:50:38 -0400

    improved

--- src/HandlebarsHelpers/In_ArrayHelper.php
+++ src/HandlebarsHelpers/In_ArrayHelper.php
@@ -10,13 +10,14 @@ class In_ArrayHelper implements Helper
 {
     public function execute(Template $template, Context $context, $args, $source)
     {
+        $found = false;
         $parsedArgs = $template->parseArguments($args);
         $needle = $context->get($parsedArgs[0]);
         $haystack = $context->get($parsedArgs[1]);
 
         if ((is_string($needle) || is_numeric($needle)) && is_array($haystack) && in_array($needle, $haystack)) {
             $template->setStopToken('else');
-            $buffer = $template->render($context);
+            $buffer = true;//$template->render($context);
             $template->setStopToken(false);
             $template->discard($context);
             $found = true;
@@ -25,7 +26,7 @@ class In_ArrayHelper implements Helper
             $template->setStopToken('else');
             $template->discard($context);
             $template->setStopToken(false);
-            $buffer = $template->render($context);
+            $buffer = false;//$template->render($context);
         }
 
         return $buffer;
