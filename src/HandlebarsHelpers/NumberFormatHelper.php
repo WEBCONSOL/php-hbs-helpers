a185a41d07cb123365371156169252dba7323368
Sothea Nim <sothea.nim@ezpizee.com>
2020-09-03 08:38:42 -0400

    updated number format

--- src/HandlebarsHelpers/NumberFormatHelper.php
+++ src/HandlebarsHelpers/NumberFormatHelper.php
@@ -13,7 +13,19 @@ class NumberFormatHelper implements Helper
         $parsedArgs = $template->parseArguments($args);
         $buffer = $context->get($parsedArgs[0]);
         if (is_numeric($buffer)) {
-            $buffer = number_format($buffer);
+            $decimals = 0;
+            $dec_point = '.';
+            $thousands_sep = ',';
+            if (isset($parsedArgs[1]) && is_numeric($context->get($parsedArgs[1]))) {
+                $decimals = $context->get($parsedArgs[1]);
+            }
+            if (isset($parsedArgs[2]) && strlen($context->get($parsedArgs[2])) === 1) {
+                $dec_point = $context->get($parsedArgs[2]);
+            }
+            if (isset($parsedArgs[3]) && strlen($context->get($parsedArgs[3])) === 1) {
+                $thousands_sep = $context->get($parsedArgs[3]);
+            }
+            $buffer = number_format($buffer, $decimals, $dec_point, $thousands_sep);
         }
         return $buffer;
     }
